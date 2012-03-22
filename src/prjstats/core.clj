63a8a07109f09db17d4bc2d0d239b8ee9688a8b7
reborg <reborg@reborg.net>
2012-03-22 20:39:16 +0000

    Just added reference to stackoverflow article

--- src/prjstats/core.clj
+++ src/prjstats/core.clj
@@ -15,6 +15,7 @@
     [""] index))
  
 ;;; start function for starting jetty
+;;; http://stackoverflow.com/questions/2706044/how-do-i-stop-jetty-server-in-clojure
 ;;; (.stop server) after starting
 (defn start [& port]
   (defonce server (run-jetty #'routes {:port (or port 8888) :join? false})))
