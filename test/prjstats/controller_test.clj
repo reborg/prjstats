5e584c5d551e162f36d9a912d01c4a09ca48a530
reborg <reborg@reborg.net>
2012-03-29 19:02:26 +0100

    Added another simple test based on string matching. Not happy about this kind of fragile accpt. Waiting to know more about tooling aroung.

--- test/prjstats/controller_test.clj
+++ test/prjstats/controller_test.clj
@@ -7,23 +7,6 @@
   "all projects available are on the page"
   (count (re-seq #"project_name" (join (:body (index "req"))))) => 4)
 
-;;(facts "about the main page"
-;;  "Bare request produces prompt"
-;;  (handler {:uri "/" :request-method :get})
-;;  => (contains {:status 200
-;;                :body #"add two numbers"})
-;;
-;;  "Valid numbers produce sum"
-;;  (handler {:uri "/" :request-method :post :params {"a" "1" "b" "2"}})
-;;  => (contains {:status 200
-;;                :body #"1 \+ 2 = 3"})
-;;
-;;  "Non-numbers produce error message"
-;;  (handler {:uri "/" :request-method :post :params {"a" "2" "b" "f"}})
-;;  => (contains {:status 200
-;;                :body #"those are not both numbers"}))
-;;
-;;(fact "Other pages produce redirect"
-;;  (handler {:uri "/anything" :request-method :get})
-;;  => (contains {:status 302,
-;;                :headers (contains {"Location" "/"})}))
+(facts "details for a specific project"
+  "all metrics are on the page for the specific project"
+  (count (re-seq #"Metrics for" (join (:body (project "/" "1"))))) => 1)
